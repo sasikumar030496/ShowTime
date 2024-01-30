7ef691ea79cc7e0cb5f87e264288537e43696878
Sasi Kumar <sasikumar030496@gmail.com>
2024-01-30 22:05:09 +0530

    Updated axios API URLs

--- src/API/Auth/index.js
+++ src/API/Auth/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const signIn= (userDetails)=>{
-  const PATH = "/mba/api/v1/auth/signin"
+  const PATH = "/showtime/api/v1/auth/signin"
   const URL = BASE_URL+PATH
   return axios.post(URL,{
         userId:userDetails.userName,
--- src/API/Movies/index.js
+++ src/API/Movies/index.js
@@ -2,13 +2,13 @@ import axios from "axios"
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const getAllMovies= ()=>{
-    const PATH = "/mba/api/v1/movies"
+    const PATH = "/showtime/api/v1/movies"
     const URL = BASE_URL+PATH
     return axios.get(URL);
 }
 
 export const getMovieDetails= (id)=>{
-    const PATH = `/mba/api/v1/movies/${id}`
+    const PATH = `/showtime/api/v1/movies/${id}`
     const URL = BASE_URL+PATH
     return axios.get(URL);
 }
